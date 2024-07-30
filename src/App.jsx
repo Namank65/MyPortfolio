b0cb42762d3aaddbca61b64ec9ac72b0cf30663f
souamya <namankumar62039@gmail.com>
2024-07-31 02:14:21 +0530

    added react icon package

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { IconContext } from "react-icons/lib";
+import logo from "./assets/logo3.png";
+import { BsTwitterX } from "react-icons/bs";
+
 function App() {
   return (
-    <div>
-      <h1 className="border text-cyan-50 border-b-2 mt-7">helo</h1>
-    </div>
+    <>
+    <IconContext.Provider
+      value={{ color: 'white', size: '50px' }}
+    >
+      <div>
+        
+        <BsTwitterX/>
+        <img src={logo} alt="Logo" className="w-48" />
+      </div>
+      {/* <h1 className="border text-cyan-50 border-b-2 mt-7">helo</h1> */}
+    </IconContext.Provider>
+    </>
   );
 }
 
