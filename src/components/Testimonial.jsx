57348434419f58872e163f1e98680de9380482b8
souamya <namankumar62039@gmail.com>
2024-09-02 01:41:24 +0530

    more user friendly

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import clint1 from "../assets/saatvik.webp";
+import clint2 from "../assets/srihari.webp";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
 import "../swiper.css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
 import "swiper/css";
 
 const Testimonial = () => {
@@ -15,21 +19,23 @@ const Testimonial = () => {
       <div className="max-w-7xl flex w-[80%] justify-center items-center text-black m-auto">
         <Swiper
           navigation={true}
-          modules={[Navigation]}
+          pagination={true}
+          clickable={true}
+          modules={[Navigation,Pagination]}
           className="mySwiper py-16"
         >
           <SwiperSlide>
-            <div className="flex cursor-grab flex-col md:flex-row gap-5 shadow-2xl shadow-slate-400 rounded-xl justify-center items-center m-auto max-w-[800px] p-11 w-[80%]">
-              <div>
+            <div className="flex cursor-grab flex-col md:flex-row gap-5 shadow-2xl shadow-slate-400 rounded-xl justify-center items-center m-auto max-w-[800px] p-8 w-[90%]">
+              <div className="max-w-screen-md flex justify-center">
                 <img
-                  className="rounded-full hover:scale-110 duration-300 ease-in-out"
-                  src="https://anuragsinghbam.netlify.app/images/saatvik.webp"
+                  className="w-[40%]  md:w-[90%]  rounded-full hover:scale-110 duration-300 ease-in-out"
+                  src={clint1}
                   alt="img"
                 />
               </div>
 
-              <div className="text-white pt-14">
-                <p className="text-left my-4 font-light ">
+              <div className="text-white md:pt-16 max-w-screen-sm">
+                <p className="text-left text-sm md:text-lg my-4 font-light ">
                   “Anurag is a talented, committed individual who will leave no
                   stone unturned in his pursuit to provide with the best. His
                   attention to detail and in-depth experience in the of web
@@ -37,36 +43,40 @@ const Testimonial = () => {
                   skills in the field, and I hope to see all the great projects
                   coming up!”
                 </p>
-                <h2 className="text-right p-2 font-semibold italic text-3xl">
+                <h2 className="text-right p-2 font-semibold italic text-xl md:text-3xl">
                   Satvik Nagpal
                 </h2>
-                <h5 className="text-right">Founder, EzyGrad</h5>
+                <h5 className="text-right text-sm md:text-base ">Founder, EzyGrad</h5>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="flex cursor-grab flex-col md:flex-row gap-5 shadow-2xl shadow-slate-400  rounded-xl  justify-center items-center m-auto max-w-[800px] p-11 w-[80%]">
-              <div className="">
+            <div className="flex cursor-grab flex-col md:flex-row gap-5 shadow-2xl shadow-slate-400 rounded-xl justify-center items-center m-auto max-w-[800px] p-8 w-[90%]">
+              <div className="max-w-screen-md flex justify-center">
                 <img
-                  className="rounded-full hover:scale-110 duration-300 ease-in-out"
-                  src="https://anuragsinghbam.netlify.app/images/Srihari.webp"
+                  className="w-[40%]  md:w-[90%] rounded-full hover:scale-110 duration-300 ease-in-out"
+                  src={clint2}
                   alt="img"
                 />
               </div>
 
-              <div className="text-white pt-14">
-                <p className="text-left my-4 font-light ">
-                  “I worked with Anurag to make my website. I am speechless by
-                  looking at his work ethic and dedication. Working with him was
-                  the best decision I made.”
+              <div className="text-white md:pt-16 max-w-screen-sm">
+                <p className="text-left text-sm md:text-lg my-4 font-light ">
+                  “Anurag is a talented, committed individual who will leave no
+                  stone unturned in his pursuit to provide with the best. His
+                  attention to detail and in-depth experience in the of web
+                  development is indeed commendable. He has exhibited exemplary
+                  skills in the field, and I hope to see all the great projects
+                  coming up!”
                 </p>
-                <h2 className="text-right p-2 font-semibold italic text-3xl ">
-                  Srihari Kestur{" "}
+                <h2 className="text-right p-2 font-semibold italic text-xl md:text-3xl">
+                Srihari Kestur 
                 </h2>
-                <h5 className="text-right">Founder Harigurus</h5>
+                <h5 className="text-right text-sm md:text-base ">Founder, Harigurus</h5>
               </div>
             </div>
           </SwiperSlide>
+
         </Swiper>
       </div>
     </div>
