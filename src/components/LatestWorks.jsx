0ea58ae262986ab8f3a41f56d076764f978903b8
souamya <namankumar62039@gmail.com>
2024-11-15 23:15:01 +0530

    tried to change the orintiation

--- src/components/LatestWorks.jsx
+++ src/components/LatestWorks.jsx
@@ -4,7 +4,7 @@ import { FaArrowDownLong } from "react-icons/fa6";
 const LatestWorks = () => {
   return (
     <div className="flex justify-center text-white h-24">
-      <button className="py-1 absolute sm:py-2 px-2 sm:px-8 font-medium sm:font-bold rounded-md bg-gradient-to-l from-[#8c52ff] to-[#341671] hover:scale-110 duration-150 group ">
+      <button onClick={window.scroll(0,750)} className="py-1 absolute sm:py-2 px-2 sm:px-8 font-medium sm:font-bold rounded-md bg-gradient-to-l from-[#8c52ff] to-[#341671] hover:scale-110 duration-150 group ">
         Latest Works
         <FaArrowDownLong className="text-white size-5 ml-9 absolute md:group-hover:mt-2 group-hover:mt-1 ease-in-out duration-300 mt-0 sm:mt-1" />
       </button>
